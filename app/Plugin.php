a472898bae906baabc6f989124abac45886c7ace
mahmoud-adel44 <mhmoud.adel18@yahoo.com>
2022-05-02 20:01:25 +0200

    start

---
+++ app/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Corp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plugin extends Model
+{
+    //
+	protected $fillable = ['alias','activated'];
+	protected $table = 'plugins';
+
+
+
+}
